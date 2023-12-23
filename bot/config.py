c79bf5efefd6c44f9a3aec4620ac14597ef45201
Ibrahim Khan <108752662+luciferworld@users.noreply.github.com>
2023-12-23 19:58:46 +0500

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -5,15 +5,15 @@ class Telegram:
     API_HASH = env.get("TELEGRAM_API_HASH", "9de2fcd18b25deed06adf855fcd181ed")
     OWNER_ID = int(env.get("OWNER_ID", 5390385209))
     ALLOWED_USER_IDS = env.get("ALLOWED_USER_IDS", "").split()
-    BOT_USERNAME = env.get("TELEGRAM_BOT_USERNAME", "luciferfilestrem_bot")
+    BOT_USERNAME = env.get("TELEGRAM_BOT_USERNAME", "Liciferfiletolink_bot")
     BOT_TOKEN = env.get("TELEGRAM_BOT_TOKEN", "6988190080:AAHc9v-B5-bd3vJ-ooKLNZ-rnv9GFogXiNI")
     CHANNEL_ID = int(env.get("TELEGRAM_CHANNEL_ID", -1001805072460))
     SECRET_CODE_LENGTH = int(env.get("SECRET_CODE_LENGTH", 12))
 
 class Server:
-    BASE_URL = env.get("BASE_URL", "http://20.244.6.103:6985")
+    BASE_URL = env.get("BASE_URL", "http://filestream.lucifercloud.me:8080")
     BIND_ADDRESS = env.get("BIND_ADDRESS", "0.0.0.0")
-    PORT = int(env.get("PORT", 6985))
+    PORT = int(env.get("PORT", 8080))
 
 # LOGGING CONFIGURATION
 LOGGER_CONFIG_JSON = {
