b1244a31f3c91c26630ae117406dd97a0ee9f467
Ibrahim Khan <108752662+luciferworld@users.noreply.github.com>
2023-12-23 20:21:36 +0500

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -11,7 +11,7 @@ class Telegram:
     SECRET_CODE_LENGTH = int(env.get("SECRET_CODE_LENGTH", 12))
 
 class Server:
-    BASE_URL = env.get("BASE_URL", "https://filestream.lucifercloud.me:443")
+    BASE_URL = env.get("BASE_URL", "https://filetolink-9hv4.onrender.com/:443")
     BIND_ADDRESS = env.get("BIND_ADDRESS", "0.0.0.0")
     PORT = int(env.get("PORT", 443))
 
